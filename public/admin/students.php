74df12fd05d3ba92f042618a719b343cfd0908af
Victor Bruce <victorbruce82@gmail.com>
2018-04-27 12:12:37 +0000

    Added file to handle the CRUD functionalities of students

---
+++ public/admin/students.php
@@ -0,0 +1,17 @@
+<?php
+	include("../../includes/sessions.php");
+	include("../../includes/db_connection.php");
+	include("../../includes/functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Admin Panel</title>
+</head>
+<body>
+	<h1>Manage Students</h1>
+	<a href="">Add Students</a>
+	
+</body>
+</html>
